0845c251f73d08eb69cf1fcbc8261be849d6584f
robert-birliga <alexandru.birliga@takeofflabs.com>
2021-11-26 13:05:52 +0200

    Add g2.txt

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from console_application import ConsoleApplication
 
 if __name__ == "__main__":
-    console_application = ConsoleApplication('resources/gtest.txt')
+    console_application = ConsoleApplication('resources/g2.txt')
     console_application.run()
